e74ca71f55933271cc04a4b8e29e2489595d2246
Nitin <nitinchouhan9111@gmail.com>
2026-06-06 15:18:35 +0530

    added the try blockes

--- app.py
+++ app.py
@@ -9,17 +9,24 @@ dotenv.load_dotenv()
 # API key 
 client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 # route for the page where users can input the topic they want to study
-@app.route("/Topic")
+
+@app.route("/")
 def Topic():
     return render_template("index.html")
 #route for the page where the response from the Gemini API will be displayed
 @app.route("/study", methods=["POST"])
 def study():
-    topic = request.form.get("topic")
-    response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=f"Explain the topic of {topic} in simple way and in bullet points and give 3 questions to test understanding of the topic."
-)
-    return render_template("study.html", response=markdown.markdown(response.text))
+    try:
+        topic = request.form.get("topic")
+        response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"Explain the topic of {topic} in simple way and in bullet points and give 3 questions to test understanding of the topic."
+    )
+        return render_template("study.html", response=markdown.markdown(response.text))
+    
+    except genai.errors.ClientError as e:
+        return render_template("study.html", response=f"API error: {str(e)}")
+    except Exception as e:
+        return render_template("study.html", response=f"Something went wrong: {str(e)}")
 if __name__ == "__main__":
     app.run(debug=True)
